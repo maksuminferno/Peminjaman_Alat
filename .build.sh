530f50b136935da53b67375b312344d65e247fd1
maksuminferno <kevinpermadi73@gmail.com>
2026-04-07 20:37:39 -0700

    Add Linux-safe build script

--- .build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Buat folder bootstrap/cache jika belum ada
-mkdir -p bootstrap/cache
-chmod -R 775 bootstrap/cache
-
-# Buat folder storage agar Laravel bisa menulis cache & logs
-mkdir -p storage/framework/cache/data
-chmod -R 775 storage4
