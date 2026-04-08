13e7efcb573eaccf738f19a1ea200a4cb7882ea9
maksuminferno <kevinpermadi73@gmail.com>
2026-04-07 19:42:37 -0700

    Add build hook and other updates for Laravel Cloud deploy

--- .build.sh
+++ .build.sh
@@ -2,10 +2,8 @@
 
 # Buat folder bootstrap/cache jika belum ada
 mkdir -p bootstrap/cache
-
-# Berikan permission menulis
 chmod -R 775 bootstrap/cache
 
-# Buat folder storage jika belum ada
+# Buat folder storage agar Laravel bisa menulis cache & logs
 mkdir -p storage/framework/cache/data
 chmod -R 775 storage
